823198cd1b26ba0f7f01193b24a9bc7c8a636216
Thomas Prime <thopri@noc.ac.uk>
2019-11-01 14:06:36 +0000

    updated profile.py

--- pynemo/profile.py
+++ pynemo/profile.py
@@ -92,12 +92,65 @@ def process_bdy(setup_filepath=0, mask_gui=False):
     
     logger.info('Start NRCT Logging: '+time.asctime())
     logger.info('============================================')
-    
-    SourceCoord = source_coord.SourceCoord()
-    DstCoord    = dst_coord.DstCoord()
 
     Setup = setup.Setup(setup_filepath) # default settings file
     settings = Setup.settings
+
+    # download CMEMS data if requested
+    
+    if settings['use_cmems'] == True:
+        
+        logger.info('starting CMEMS download process....')
+ 
+        if settings['year_end'] - settings['year_000'] > 0:
+            date_min = settings['year_000']+'-01-01'
+            date_max = settings['year_end']+'-12-31'
+        
+        days_mth = monthrange(settings['year_end'],settings['month_end'])
+        
+        date_min = str(settings['year_000'])+'-'+str(settings['month_000'])+'-01'
+        
+        date_max = str(settings['year_end'])+'-'+str(settings['month_end'])+'-'+str(days_mth[1])
+        
+        cmes_config= {
+               'ini_config_template'    : settings['cmes_config_template'],
+               'user'                   : settings['cmes_usr'],
+               'pwd'                    : settings['cmes_pwd'],
+               'motu_server'            : settings['motu_server'],  
+               'service_id'             : settings['cmes_model'],
+               'product_id'             : settings['cmes_product'],
+               'date_min'               : date_min,
+               'date_max'               : date_max,
+               'latitude_min'           : settings['latitude_min'],
+               'latitude_max'           : settings['latitude_max'],
+               'longitude_min'          : settings['longitude_min'],
+               'longitude_max'          : settings['longitude_max'],
+               'depth_min'              : settings['depth_min'],
+               'depth_max'              : settings['depth_max'],
+               'variable'               : settings['cmes_variable'],
+               'out_dir'                : settings['dst_dir'],
+               'out_name'               : settings['cmes_output'],
+               'config_out'             : settings['cmes_config']
+               }        
+        
+        chk = dl_cmems.chk_motu()
+        
+        if chk == 1:
+            logger.error('motuclient not installed, please install by running $ pip install motuclient')
+            
+        else:
+            logger.info('version ' +chk+ ' of motuclient is installed')
+        logger.info('requesting CMES download now (this can take a while)...')    
+        dl = dl_cmems.request_CMEMS(cmes_config)
+        
+        if dl == 0:
+            logger.info('CMES data downloaded successfully')
+            
+        if type(dl) == str:
+            logger.error(dl)
+
+    SourceCoord = source_coord.SourceCoord()
+    DstCoord    = dst_coord.DstCoord()
 
     logger.info('Reading grid completed')
 
@@ -216,57 +269,6 @@ def process_bdy(setup_filepath=0, mask_gui=False):
  
     unit_origin = '%d-01-01 00:00:00' %settings['base_year']
 
-    # download CMEMS data is requested
-
-    if settings['use_cmes'] == True:
- 
-        if settings['year_end'] - settings['year_000'] > 0:
-            date_min = settings['year_000']+'-01-01'
-            date_max = settings['year_end']+'-12-31'
-        
-        days_mth = monthrange(settings['year_end'],settings['month_end'])
-        
-        date_min = str(settings['year_000'])+'-'+str(settings['month_000'])+'-01'
-        
-        date_max = str(settings['year_end'])+'-'+str(settings['month_end'])+'-'+str(days_mth[1])
-        
-        cmes_config= {
-               'ini_config_template'    : settings['cmes_config_template'],
-               'user'                   : settings['cmes_usr'],
-               'pwd'                    : settings['cmes_pwd'],
-               'motu_server'            : settings['motu_server'],  
-               'service_id'             : settings['cmes_model'],
-               'product_id'             : settings['cmes_product'],
-               'date_min'               : date_min,
-               'date_max'               : date_max,
-               'latitude_min'           : settings['latitude_min'],
-               'latitude_max'           : settings['latitude_max'],
-               'longitude_min'          : settings['longitude_min'],
-               'longitude_max'          : settings['longitude_max'],
-               'depth_min'              : settings['depth_min'],
-               'depth_max'              : settings['depth_max'],
-               'variable'               : settings['cmes_variable'],
-               'out_dir'                : settings['dst_dir'],
-               'out_name'               : settings['cmes_output'],
-               'config_out'             : settings['cmes_config']
-               }        
-        
-        chk = dl_cmems.chk_motu()
-        
-        if chk == 1:
-            logger.error('motuclient not installed, please install by running $ pip install motuclient')
-            
-        else:
-            logger.info('version ' +chk+ ' of motuclient is installed')
-        logger.info('requesting CMES download now (this can take a while)...')    
-        dl = dl_cmems.request_CMEMS(cmes_config)
-        
-        if dl == 0:
-            logger.info('CMES data downloaded successfully')
-            
-        if type(dl) == str:
-            logger.error(dl)   
-    
     # Extract source data on dst grid
 
     if settings['tide']:
