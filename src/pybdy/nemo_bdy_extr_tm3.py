e06c559b2a2825c5801383c9415cf69b5ff2917e
benbar <benbar@noc.ac.uk>
2025-01-15 15:29:29 +0000

    Bug fix

--- src/pybdy/nemo_bdy_extr_tm3.py
+++ src/pybdy/nemo_bdy_extr_tm3.py
@@ -1087,11 +1087,11 @@ class Extract:
                     if self.key_vec is True and self.rot_dir == "j":
                         self.d_bdy[self.var_nam[vn + 1]][year]["data"][
                             int(f - first_date), :, chunk_d
-                        ] = np.array([data_out])
+                        ] = data_out.T
                     else:
                         self.d_bdy[self.var_nam[vn]][year]["data"][
                             int(f - first_date), :, chunk_d
-                        ] = np.array([data_out])
+                        ] = data_out.T
 
                     # add data to self.d_bdy called entry
                     # TODO change this so holding array is already set up
