80cfe83352ab69aa7c383eeb4af12d4b5990fd2f
benbar <benbar@noc.ac.uk>
2025-02-11 17:42:02 +0000

    Started tests

---
+++ tests/test_hgr.py
@@ -0,0 +1,35 @@
+# ===================================================================
+# The contents of this file are dedicated to the public domain.  To
+# the extent that dedication to the public domain is not available,
+# everyone is granted a worldwide, perpetual, royalty-free,
+# non-exclusive license to exercise all rights associated with the
+# contents of this file for any purpose whatsoever.
+# No rights are reserved.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ===================================================================
+
+"""
+Created on Thu Dec 22 18:03:00 2024.
+
+@author James Harle
+@author Benjamin Barton
+@author Ryan Patmore
+"""
+
+# External imports
+
+# Internal imports
+from src.grid import hgr
+
+
+def test_h_grid():
+    hgr.H_grid()
+    assert False
