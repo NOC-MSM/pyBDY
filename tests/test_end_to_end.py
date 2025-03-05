703635b038b4679082e609f3d7e1bdb1257402a8
benbar <benbar@noc.ac.uk>
2025-03-05 10:44:56 +0000

    Started building test data for the end to end test

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -24,10 +24,51 @@ Created on Thu Feb 13 18:03:00 2025.
 """
 
 # External imports
+import os
 
+import numpy as np
 
 # Internal imports
+from tests import synth_bathymetry, synth_zgrid
 
 
 def test_all():
+    path1 = generate_test_case1()
+    path2 = generate_test_case2()
+    os.remove(path1)
+    os.remove(path2)
+
     assert False
+
+
+def generate_test_case1():
+    # Generate a synthetic test case
+    path1 = "./tests/synth_test_full.nc"
+
+    lon_t = np.arange(-20, 1, 1)
+    lat_t = np.arange(40, 53, 0.5)
+    ppe1 = np.zeros((len(lon_t))) + 1
+    ppe2 = np.zeros((len(lat_t))) + 0.5
+    synth = synth_bathymetry.Bathymetry(ppe1, ppe2, ppglam0=lon_t[0], ppgphi0=lat_t[0])
+    synth = synth.sea_mount(depth=1000, stiff=1000)
+
+    n_zlevel = 20
+    gdept, gdepw = synth_zgrid.synth_sco(synth["Bathymetry"], n_zlevel)
+
+    synth.to_netcdf(path1)
+    return path1
+
+
+def generate_test_case2():
+    # Generate a synthetic test case
+    path2 = "./tests/synth_test_sub.nc"
+
+    lon_t = np.arange(-20, 1, 1)
+    lat_t = np.arange(40, 53, 0.5)
+    ppe1 = np.zeros((len(lon_t))) + 1
+    ppe2 = np.zeros((len(lat_t))) + 0.5
+    synth = synth_bathymetry.Bathymetry(ppe1, ppe2, ppglam0=lon_t[0], ppgphi0=lat_t[0])
+    synth = synth.sea_mount(depth=1000, stiff=1000)
+
+    synth.to_netcdf(path2)
+    return path2
